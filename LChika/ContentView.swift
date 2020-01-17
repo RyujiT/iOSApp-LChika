127a817acd42b4437a68a64285580abf2a957bf4
rtaba <the.sun.also.rises.2nd@gmail.com>
2020-01-17 21:02:01 +0900

    Initial Commit

---
+++ LChika/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LChika
+//
+//  Created by 田場隆司 on 2020/01/17.
+//  Copyright © 2020 R.Taba. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
